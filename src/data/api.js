b4e24cc13d1cd8f0cfd6cbf965fbdeea0acb5b92
emanuela <emanuela.markovic@hotmail.com>
2024-04-26 10:16:41 +0200

    fix: update name to api_url in endpoints constant

--- src/data/api.js
+++ src/data/api.js
@@ -6,13 +6,13 @@ const API_URL = developmentMode ? "http://localhost:4444" : productionMode;
 const endpoints = {
   register: `${API_URL}/users/register`,
   login: `${API_URL}/users/login`,
-  forgotPassword: `${BASE_URL}/users/forgot-password`,
+  forgotPassword: `${API_URL}/users/forgot-password`,
   logout: `${API_URL}/users/logout`,
   profile: `${API_URL}/users/profile`,
-  todos: `${BASE_URL}/todos`,
-  calendar: `${BASE_URL}/calendar`,
-  timer: `${BASE_URL}/timer`,
-  notepad: `${BASE_URL}/notepad`,
+  todos: `${API_URL}/todos`,
+  calendar: `${API_URL}/calendar`,
+  timer: `${API_URL}/timer`,
+  notepad: `${API_URL}/notepad`,
 };
 
 export default { endpoints, API_URL, productionMode };
