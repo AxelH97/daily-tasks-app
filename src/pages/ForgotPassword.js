1671c8d2b97dee9402ca6d8b5148bddf5131c8c9
emanuela <emanuela.markovic@hotmail.com>
2024-05-03 10:27:28 +0200

    feature: forgot-password

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,18 +1,55 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
+import { Text, View, TextInput, Pressable } from "react-native";
+import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import styles from "../style/loginStyle";
-
-// import { useUsersContext } from "../contexts/UserContextProvider";
+import { useNavigation } from "@react-navigation/native";
+import { paths } from "../features/navigation/routing/paths";
+import { useUsersContext } from "../context/UserContext";
+import { API_URL } from "../data/api";
 
 const ForgotPassword = () => {
-  //   const { user } = useUsersContext();
+  const { user } = useUsersContext();
+  const [email, setEmail] = useState("");
+  const navigation = useNavigation();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${API_URL}/users/forgot-password`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      navigation.navigate(paths.login);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <View style={styles.inputContainer}>
-      <MaterialIcons style={styles.icon} name="email" size={20} color="gray" />
-      <TextInput style={styles.input} placeholder="enter your email" />
-    </View>
+    <>
+      <View style={styles.inputContainer}>
+        <MaterialIcons
+          style={styles.icon}
+          name="email"
+          size={20}
+          color="gray"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="enter your email"
+          onChangeText={(text) => setEmail(text)}
+        />
+      </View>
+      <Pressable onPress={fetchData} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Send</Text>
+      </Pressable>
+    </>
   );
 };
 
