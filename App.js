268ecb9d2babcefa46511fe5e56ff83d3912f0fe
emanuela <emanuela.markovic@hotmail.com>
2024-05-03 11:51:37 +0200

    delete: unused code

--- App.js
+++ App.js
@@ -2,10 +2,8 @@
 
 import React from "react";
 import AppProvider from "./src/context/AppProvider";
-import { createStackNavigator } from "@react-navigation/stack";
 import Routes from "./src/features/navigation/layout/Routes";
 
-const Stack = createStackNavigator();
 const App = () => {
   return (
     <AppProvider>
