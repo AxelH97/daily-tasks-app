eb18d396a38797beaba6c362532af766b4007a57
Melle1986 <melledom@googlemail.com>
2024-05-14 09:24:08 +0200

    Fix-calendar

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-
+import Calendar from './components/Calendar';
 
 export default function App() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Calendar onSelectDate={setSelectedDate} selected={selectedDate} />
+      <StatusBar style="auto" />
     </View>
   );
 }
