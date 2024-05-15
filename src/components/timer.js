b855e7351c87d757ce240208224abc37c5985bd3
AxelH97 <hilde.axel@web.de>
2024-05-15 13:16:55 +0200

    fix: style in a seperate folder

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import styles from "../style/timerStyle";
 
 const Timer = () => {
   const [hours, setHours] = useState("0");
@@ -79,41 +80,5 @@ const Timer = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    marginBottom: 20,
-  },
-  input: {
-    width: 60,
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    marginRight: 5,
-    textAlign: "center",
-  },
-  separator: {
-    alignSelf: "center",
-    fontSize: 20,
-    marginHorizontal: 5,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    marginBottom: 20,
-  },
-  timer: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
 
 export default Timer;
