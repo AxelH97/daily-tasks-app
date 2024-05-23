71374e9260da0cad3fa1a3a6a9f76d6a9831521a
emanuela <emanuela.markovic@hotmail.com>
2024-05-23 20:30:41 +0200

    feature: import User and Task provider

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import AppProvider from "./src/context/AppProvider";
+import UserContextProvider from "./src/context/UserContext";
+import TaskContextProvider from "./src/context/TasksContext";
 import Routes from "./src/features/navigation/layout/Routes";
 
 const App = () => {
   return (
-    <AppProvider>
-      <Routes />
-    </AppProvider>
+    <UserContextProvider>
+      <TaskContextProvider>
+        <AppProvider>
+          <Routes />
+        </AppProvider>
+      </TaskContextProvider>
+    </UserContextProvider>
   );
 };
 export default App;
