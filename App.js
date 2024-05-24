03b62346473d90fdbc5447e9b25b1446ca3e10b7
emanuela <emanuela.markovic@hotmail.com>
2024-05-24 11:29:00 +0200

    delete: unnecessery code

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import AppProvider from "./src/context/AppProvider";
-import UserContextProvider from "./src/context/UserContext";
-import TaskContextProvider from "./src/context/TasksContext";
 import Routes from "./src/features/navigation/layout/Routes";
 
 const App = () => {
   return (
-    <UserContextProvider>
-      <TaskContextProvider>
-        <AppProvider>
-          <Routes />
-        </AppProvider>
-      </TaskContextProvider>
-    </UserContextProvider>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
   );
 };
 export default App;
