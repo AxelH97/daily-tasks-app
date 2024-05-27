c3627e540af6c505f0a8079b70eecacbe1edae51
emanuela <emanuela.markovic@hotmail.com>
2024-05-27 13:12:43 +0200

    feature:  KeyboardAvoidingView,  ScrollView and errorText

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,8 +8,13 @@ import {
   Pressable,
   TouchableOpacity,
   Alert,
+  ScrollView, 
+  Platform, 
 } from "react-native";
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons,} from "@expo/vector-icons";
+import { MaterialCommunityIcons} from "@expo/vector-icons";
+
+MaterialCommunityIcons 
 import styles from "../style/loginStyle";
 import { useNavigation } from "@react-navigation/native";
 import { paths } from "../features/navigation/routing/paths";
@@ -19,40 +24,63 @@ import axios from "axios";
 
 const Login = () => {
   const navigation = useNavigation();
-  const { user, dispatchUser } = useUsersContext();
+  const { dispatchUser } = useUsersContext();
   const [secureEntry, setSecureEntry] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [buttonText, setButtonText] = useState("Login");
 
   const toggleSecureEntry = () => {
     setSecureEntry((prevSecureEntry) => !prevSecureEntry);
   };
 
+  const handleEmailChange = (text) => {
+    setEmail(text);
+    setIsEmailValid(text.includes("@"));
+  };
+
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+    setIsPasswordValid(text.length >= 8);
+  };
+
   const fetchData = async () => {
+    setLoading(true);
+    setButtonText("Logging in...");
     try {
       const response = await axios.post(
         `${API_URL}/users/login`,
-        {
-          email: email,
-          password: password,
-        },
-        {
-          withCredentials: true,
-        }
+        { email, password },
+        { withCredentials: true }
       );
-
-      if (!response.status === 200) {
+console.log('response:',response);
+      if (response.status !== 200) {
         const errorData = response.data;
         throw new Error(errorData.message || "Login failed");
       }
 
       const data = response.data;
-      console.log(data);
+      console.log('data:',data);
       dispatchUser({ type: "login_success", payload: data });
       navigation.navigate(paths.todos);
     } catch (error) {
       console.error(error);
       Alert.alert("Login Failed");
+    } finally {
+      setLoading(false);
+      setButtonText("Login");
+    }
+  };
+
+  const handleAuthentication = (event) => {
+    event.preventDefault();
+    if (isEmailValid && isPasswordValid) {
+      fetchData();
+    } else {
+      Alert.alert("Invalid input", "Please check your email and password.");
     }
   };
 
@@ -61,76 +89,100 @@ const Login = () => {
       <View style={{ marginTop: 80 }}>
         <Text style={styles.title}>Daily-Tasks</Text>
       </View>
-      <KeyboardAvoidingView>
-        <View style={{ alignItems: "center" }}>
-          <Text style={styles.loginText}>Log in to your account</Text>
-        </View>
-        <View style={{ marginTop: 70 }}>
-          <View style={styles.inputContainer}>
-            <MaterialIcons
-              style={styles.icon}
-              name="email"
-              size={20}
-              color="gray"
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="enter your email"
-              onChangeText={(text) => setEmail(text)}
-            />
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView keyboardDismissMode="on-drag">
+          <View style={{ alignItems: "center" }}>
+            <Text style={styles.loginText}>Log in to your account</Text>
           </View>
-          <View style={styles.passwordContainer}>
-            <MaterialIcons
-              style={styles.icon}
-              name="lock"
-              size={20}
-              color="gray"
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="enter your password"
-              secureTextEntry={secureEntry}
-              onChangeText={(text) => setPassword(text)}
-            />
-            <TouchableOpacity>
-              <MaterialCommunityIcons
+          <View style={{ marginTop: 70 }}>
+            <View style={styles.inputContainer}>
+              <MaterialIcons
+                style={styles.icon}
+                name="email"
+                size={20}
+                color="gray"
+              />
+              <TextInput
+                style={[styles.input, !isEmailValid && { borderColor: "red" }]}
+                placeholder="enter your email"
+                onChangeText={handleEmailChange}
+                value={email}
+                keyboardType="email-address"
+              />
+            </View>
+            {!isEmailValid && (
+              <Text style={styles.errorText}>Invalid email</Text>
+            )}
+            <View style={styles.passwordContainer}>
+              <MaterialIcons
+                style={styles.icon}
+                name="lock"
+                size={20}
+                color="gray"
+              />
+              <TextInput
+                style={[
+                  styles.input,
+                  !isPasswordValid && { borderColor: "red" },
+                ]}
+                placeholder="enter your password"
+                secureTextEntry={secureEntry}
+                onChangeText={handlePasswordChange}
+                value={password}
+              />
+              <TouchableOpacity
                 onPress={toggleSecureEntry}
                 style={{ marginRight: 8 }}
-                name={secureEntry ? "eye-outline" : "eye-off-outline"}
-                size={24}
-                color="grey"
-              />
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 12,
-              justifyContent: "space-between",
-            }}
-          >
-            <Text>Keep me logged in</Text>
-            <Text
-              onPress={() => navigation.navigate(paths.forgotPassword)}
-              style={{ color: "#874CCC", fontWeight: "500" }}
+              >
+                <MaterialCommunityIcons
+                  name={secureEntry ? "eye-outline" : "eye-off-outline"}
+                  size={24}
+                  color="grey"
+                />
+              </TouchableOpacity>
+            </View>
+            {!isPasswordValid && (
+              <Text style={styles.errorText}>
+                Password must be at least 8 characters
+              </Text>
+            )}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 12,
+                justifyContent: "space-between",
+              }}
             >
-              Forgot Password
-            </Text>
-          </View>
-          <View style={{ marginTop: 60 }} />
-          <Pressable onPress={fetchData} style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>Login</Text>
-          </Pressable>
-          <Pressable style={{ marginTop: 15 }}>
-            <Text
-              onPress={() => navigation.navigate(paths.register)}
-              style={styles.signupText}
+              <Text>Keep me logged in</Text>
+              {/* <Text
+                onPress={() => navigation.navigate(paths.forgotPassword)}
+                style={{ color: "#874CCC", fontWeight: "500" }}
+              >
+                Forgot Password
+              </Text> */}
+            </View>
+            <View style={{ marginTop: 60 }} />
+            <Pressable
+              onPress={handleAuthentication}
+              style={styles.buttonContainer}
+              disabled={loading}
             >
-              Don't have an account? Sign up
-            </Text>
-          </Pressable>
-        </View>
+              <Text style={styles.buttonText}>{buttonText}</Text>
+            </Pressable>
+            <Pressable style={{ marginTop: 15 }}>
+              <Text
+                onPress={() => navigation.navigate(paths.register)}
+                style={styles.signupText}
+              >
+                Don't have an account? Sign up
+              </Text>
+            </Pressable>
+          </View>
+        </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
