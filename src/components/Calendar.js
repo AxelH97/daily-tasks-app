a7a357f47069209507b8d392ec838dfae2ea12f5
emanuela <emanuela.markovic@hotmail.com>
2024-06-05 08:58:14 +0200

    fix: rename name png

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,71 +1,89 @@
-import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { Agenda } from 'react-native-calendars';
-import { useUsersContext } from '../context/UserContext';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { Calendar } from "react-native-calendars";
+import axios from "axios";
+import { FontAwesome, Feather, MaterialIcons } from "@expo/vector-icons";
+import { API_URL } from "../data/api";
 
 const CalendarComponent = () => {
-  const [items, setItems] = useState({});
-  const { user } = useUsersContext();
-  const userId = user._id;
-  console.log("userId", userId);
+  const today = moment().format("YYYY-MM-DD");
+  const [selectedDate, setSelectedDate] = useState(today);
+  const [todos, setTodos] = useState([]);
+
+  const fetchCompletedTodos = async () => {
+    try {
+      const response = await axios.get(
+        `${API_URL}/todos/completed/${selectedDate}`
+      );
+      const completedTodos = response.data.completedTodos || [];
+      setTodos(completedTodos);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(`http://localhost:4444/users/${userId}/todos`);
-      const data = await response.json();
-      const personalTask = data.todos;
-      console.log("data", personalTask);
+    fetchCompletedTodos();
+  }, [selectedDate]);
 
-      const mappedData = personalTask.map((task) => {
-        return {
-          ...task,
-          date: format(new Date(task.dueDate), 'yyyy-MM-dd'),
-        };
-      });
-
-      const reduced = mappedData.reduce((acc, currentItem) => {
-        const { date, ...coolItem } = currentItem;
-        if (!acc[date]) {
-          acc[date] = [];
-        }
-        acc[date].push(coolItem);
-        return acc;
-      }, {});
-
-      setItems(reduced);
-    };
-
-    getData();
-  }, []);
-
-  const renderItem = (item) => {
-    return (
-      <View style={styles.itemContainer}>
-        <Text>{item.title}</Text>
-        <Text>{item.status}</Text>
-      </View>
-    );
+  const handleDayPress = (day) => {
+    setSelectedDate(day.dateString);
   };
 
   return (
-    <SafeAreaView style={styles.safe}>
-      <Agenda items={items} renderItem={renderItem} />
-    </SafeAreaView>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Calendar
+        onDayPress={handleDayPress}
+        markedDates={{
+          [selectedDate]: { selected: true, selectedColor: "#7CB9E8" },
+        }}
+      />
+
+      <View style={{ marginTop: 20 }} />
+
+      <View style={styles.header}>
+        <Text>Completed Tasks</Text>
+        <MaterialIcons name="arrow-drop-down" size={24} color="black" />
+      </View>
+
+      {todos.map((item, index) => (
+        <Pressable style={styles.todoItem} key={index}>
+          <View style={styles.todoContent}>
+            <FontAwesome name="circle" size={18} color="gray" />
+            <Text style={styles.todoText}>{item.title}</Text>
+            <Feather name="flag" size={20} color="gray" />
+          </View>
+        </Pressable>
+      ))}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  safe: {
-    flex: 1,
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+    marginVertical: 10,
+    marginHorizontal: 10,
   },
-  itemContainer: {
-    backgroundColor: 'white',
-    margin: 5,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
+  todoItem: {
+    backgroundColor: "#E0E0E0",
+    padding: 10,
+    borderRadius: 7,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  todoContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  todoText: {
     flex: 1,
+    textDecorationLine: "line-through",
+    color: "gray",
   },
 });
 
